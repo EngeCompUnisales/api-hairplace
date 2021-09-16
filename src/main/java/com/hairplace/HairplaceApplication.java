c114900353080375743ac3178c060cf357e8820f
Jonathan Trabach <52935711+Jonathan-trabach@users.noreply.github.com>
2021-09-15 23:21:21 -0300

    add gitgnore

--- src/main/java/com/hairplace/HairplaceApplication.java
+++ src/main/java/com/hairplace/HairplaceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HairplaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HairplaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HairplaceApplication.class, args);
+    }
 
 }
