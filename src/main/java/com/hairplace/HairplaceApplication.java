0ec9e0932708650bb0a8d66c0069f23d8f55b306
Jonathan Trabach <52935711+Jonathan-trabach@users.noreply.github.com>
2021-09-15 23:15:09 -0300

    initial Spring

---
+++ src/main/java/com/hairplace/HairplaceApplication.java
@@ -0,0 +1,13 @@
+package com.hairplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hairplace/HairplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hairplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
