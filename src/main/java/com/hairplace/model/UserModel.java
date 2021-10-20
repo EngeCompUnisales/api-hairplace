7f0f3eabfcfc919d023fd47ea3828812876dcfa4
Aluno <Aluno@Souunisales.com.br>
2021-10-20 20:36:49 -0300

    tabela user

---
+++ src/main/java/com/hairplace/model/UserModel.java
@@ -0,0 +1,37 @@
+package com.hairplace.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Table(name = "servico")
+public class ServicoModel implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "id", unique = true, nullable = false)
+    private long id;
+
+    @Column(name = "nome", unique = true, nullable = false)
+    private String nome;
+
+    @Column(name = "preco", unique = false, nullable = false)
+    private long preco;
+
+
+}
