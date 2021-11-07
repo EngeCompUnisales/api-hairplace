b7d41b53be283f679a9bd39b39bac6b4f553a9af
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-11-07 15:57:52 -0300

    Método findEstabelecimentoByIdUser

--- src/main/java/com/hairplace/repository/EstabelecimentoRepository.java
+++ src/main/java/com/hairplace/repository/EstabelecimentoRepository.java
@@ -2,9 +2,13 @@ package com.hairplace.repository;
 
 import com.hairplace.model.EstabelecimentoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EstabelecimentoRepository extends JpaRepository<EstabelecimentoModel, Long>{
 
+	@Query("select e from estabelecimento e join usuario u on e.id_responsavel = u.id where u.id = %?1")
+	EstabelecimentoModel findEstabelecimentoByIdUser(long idUser);
+	
 }
